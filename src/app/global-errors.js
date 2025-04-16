9ed7bfaf0f59a4c5d8d4563573da07123dd56af6
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-04-16 15:53:54 +0200

    Global error boundary added

---
+++ src/app/global-errors.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+
+function GlobalError() {
+  return (
+    <html>
+      <body>
+      <div>GlobalError</div>
+      </body>
+    </html>
+  )
+}
+
+export default GlobalError
