bca2ea0c4c10b511b731b958df1dff6676e044f4
Rebecka <rebecka.rl.larsson@gmail.com>
2025-04-16 14:14:16 +0100

    Project intialized with Next js

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='text-6xl h-screen flex items-center justify-center'>Homepage</div>
+  )
+}
+
+export default HomePage
